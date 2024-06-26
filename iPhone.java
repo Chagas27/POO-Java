c859f423a2a92be7004934c63b6a5fb75bb37b70
Giovana Chagas Hider <131221699+Chagas27@users.noreply.github.com>
2024-06-26 20:54:53 -0300

    Add files via upload

---
+++ iPhone.java
@@ -0,0 +1,19 @@
+
+public class iPhone {
+     public static void main(String[] args) {
+		ReprodutorMusical rn = new ReprodutorMusical();
+		//rn.selecionarMusica();
+		rn.tocar();
+		rn.pausar();
+		
+		NavegadorInternet ni = new NavegadorInternet();
+		//ni.exibirPagina();
+		ni.adicionarNovaAba();
+		ni.atualizarPagina();
+		
+		AparelhoTelefonico at = new AparelhoTelefonico();
+		//at.ligar();
+		at.atender();
+		at.iniciarCorreioVoz();
+	}
+}
